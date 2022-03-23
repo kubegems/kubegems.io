82bb9fade68aeba551d90df5ea5c574952a4da8c
leon.chen <leon.chen@cloudminds.com>
2022-03-23 17:12:27 +0800

    add member's github address

--- src/components/Team/components/Member/index.tsx
+++ src/components/Team/components/Member/index.tsx
@@ -1,15 +1,18 @@
+import Link from '@docusaurus/Link';
 import React from 'react';
 import styles from './index.module.scss';
 
 const Member = ({ avatar, name, post }): JSX.Element => {
   return (
     <div className={styles.member}>
-      <img src={avatar} alt={name} className={styles.member__avatar} />
+      <Link to={avatar.replace('.png', '')}>
+        <img src={avatar} alt={name} className={styles.member__avatar} />
+      </Link>
       <div>
         <div className={styles.member__name}>{name}</div>
         <div className={styles.member__post}>{post}</div>
       </div>
-    </div>
+    </div >
   )
 }
 
