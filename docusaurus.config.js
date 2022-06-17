c73d4b9ff538f9db088f8e170f765feef2e333d5
cnfatal <cnfatal@gmail.com>
2022-06-17 12:43:00 +0800

    perf(install): v1.21.0-* quick install

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: 'v1.20.0-beta.2',
+              label: 'v1.21.0',
             },
           },
           // Please change this to your repo.
