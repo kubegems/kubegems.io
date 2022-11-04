54db92fcab102d339bbe58ce8fa3433b51e5ba65
LinkMaq <maqing1213@gmail.com>
2022-11-04 11:46:46 +0800

    update video

--- src/components/Play/index.tsx
+++ src/components/Play/index.tsx
@@ -32,9 +32,8 @@ class Play extends React.Component {
             </div>
             <div className={styles.play__video}>
               <img className={styles.play__video__close} src="/img/pages/close.png" onClick={() => { this.setState({ show: 0 }) }} />
-              {/* <video className={styles.play__video__media} poster="/img/pages/video_cover.jpg" autoPlay crossOrigin="anonymous" controls src="https://kubegems.oss-cn-chengdu.aliyuncs.com/kubegems.mp4">
-              </video> */}
-              <iframe className={styles.play__video__media} frameBorder="0" title="video" src="https://player.bilibili.com/player.html?aid=689647355&bvid=BV1tm4y1c76p&cid=879512219&page=1&as_wide=1&high_quality=1&danmaku=0&t=2"></iframe>
+              { <video className={styles.play__video__media}  poster="/img/pages/videofm.jpg" crossOrigin="anonymous" controls src="http://static.kubegems.io/video/KubeGems.mp4">
+              </video> }
             </div>
           </div>
         ) : null}
