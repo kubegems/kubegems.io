7155684cbca91bf0515efe876fbc9c964cf29d87
leon.chen <imchensl@163.com>
2023-03-24 16:33:00 +0800

    remove unnecessary files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,14 @@
  * @Author: leon.chen imchensl@163.com
  * @Date: 2023-03-16 13:40:38
  * @LastEditors: leon.chen imchensl@163.com
- * @LastEditTime: 2023-03-20 15:05:33
+ * @LastEditTime: 2023-03-24 16:28:34
  * @FilePath: /kubegems.io/src/pages/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useRef } from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Welcome from '../components/Welcome'
 import Play from '../components/Play'
-import WhyKubeGems1 from '../components/WhyKubeGems1';
-import WhyKubeGems2 from '../components/WhyKubeGems2';
-import Documents from '../components/Documents';
-import Resources from '../components/Resources';
 import PageNav from '../components/PageNav';
 import HomePage from '../components/Home'
 import CloudNative from '../components/CloudNative'
